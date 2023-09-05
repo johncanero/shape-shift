3897078575189a0b9d84c16745b39981811b422a
John Cañero <johncanero.official@gmail.com>
2023-09-05 23:46:30 +0800

    Add Favicon to ShapeShift

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'ShapeShift: Your BMI Companion',
   description: 'Your BMI Companion',
+  icons: {
+    icon: '/images/shapeShiftIcon.png',
+    shortcut: '/images/shapeShiftIcon.png',
+  },
 }
 
 export default function RootLayout({
@@ -15,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className='h-screen'>
+      <body
+        className={`${inter.className} max-w-6xl p-6 mx-auto text-gray-900 bg-gradient-to-r from-white to-teal-200 rounded-ee-3xl`}
+      >
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-   <div>
-      <h1 className='text-white text-2xl font-semibold text-center mt-12'>ShapeShift: Your BMI Companion</h1>
-   </div>
+    <div>
+      <h1 className='text-neutral-900 text-2xl font-semibold text-center mt-12'>
+        ShapeShift: Your BMI Companion
+      </h1>
+    </div>
   )
 }
