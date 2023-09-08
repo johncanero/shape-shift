e7cf64b7f4d09eab6986b4e0e27a9ccd83f7b2f5
John Cañero <johncanero.official@gmail.com>
2023-09-08 18:02:43 +0800

    Fix Export Default Function Home

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,11 +4,7 @@ import Navbar from "@/components/Navbar"
 import Header from "./components/Header"
 import Main from "./components/Main"
 
-export default function Home({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function Home() {
   return (
     <>
       <div>
