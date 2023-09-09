6dd2d43479c8b32252f2d6f4d2e84214b129eab6
John Cañero <johncanero.official@gmail.com>
2023-09-09 16:09:38 +0800

    Add Footer in Mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div>
-            <footer className="hidden text-sm text-center text-gray-500 md:block mt-12">
+            <footer className="text-sm text-center text-gray-500 md:block mt-12">
                 <Link
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://www.linkedin.com/in/johncanero/"
                 >
 
-                    <span className="mr-2 text-lg font-bold text-gray-900 dark:text-gray-100">
+                    <span className="mr-2 font-bold text-gray-900 dark:text-gray-100">
                         John Cañero
                     </span>
                 </Link>
